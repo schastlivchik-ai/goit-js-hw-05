2ee6179ce64f98f17be3617f77ca6f6003e81c18
Avramenko Yehor <avramenko.ego98@gmail.com>
2025-05-03 17:50:28 +0200

    initial commit

--- js/task-2.js
+++ js/task-2.js
@@ -1,7 +1,58 @@
-function getShippingMessage(country, price, deliveryFee) {
-    let totalPrice = price + deliveryFee;
-    return `Shipping to ${country} will cost ${totalPrice} credits`;
-}
-console.log(getShippingMessage("Australia", 120, 50));
-console.log(getShippingMessage("Germany", 80, 20));
-console.log(getShippingMessage("Sweden", 100, 20));
+const allUsers = [
+    {
+      name: "Moore Hensley",
+      friends: ["Sharron Pace"]
+    },
+    {
+      name: "Sharlene Bush",
+      friends: ["Briana Decker", "Sharron Pace"]
+    },
+    {
+      name: "Ross Vazquez",
+      friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"]
+    },
+    {
+      name: "Elma Head",
+      friends: ["Goldie Gentry", "Aisha Tran"]
+    },
+    {
+      name: "Carey Barr",
+      friends: ["Jordan Sampson", "Eddie Strong"]
+    },
+    {
+      name: "Blackburn Dotson",
+      friends: ["Jacklyn Lucas", "Linda Chapman"]
+    },
+    {
+      name: "Sheree Anthony",
+      friends: ["Goldie Gentry", "Briana Decker"]
+    }
+  ];
+  
+const getUsersWithFriend = (users, friendName) => users.filter((user) => user.friends.includes(friendName))
+
+  console.log(getUsersWithFriend(allUsers, "Briana Decker")); 
+  // [
+  //   {
+  //     name: "Sharlene Bush",
+  //     friends: ["Briana Decker", "Sharron Pace"]
+  //   },
+  //   {
+  //     name: "Sheree Anthony",
+  //     friends: ["Goldie Gentry", "Briana Decker"]
+  //   }
+  // ]
+  
+  console.log(getUsersWithFriend(allUsers, "Goldie Gentry"));
+  // [
+  //   {
+  //     name: "Elma Head",
+  //     friends: ["Goldie Gentry", "Aisha Tran"]
+  //   },
+  //   {
+  //     name: "Sheree Anthony",
+  //     friends: ["Goldie Gentry", "Briana Decker"]
+  //   }
+  // ]
+  
+  console.log(getUsersWithFriend(allUsers, "Adrian Cross" )); // []
